7817b68faabfdecf6afee9b405c5b6e8beb63d9a
kicaru <kicaur00@gmail.com>
2018-12-23 16:57:13 +0700

    first commit

---
+++ cos2103/src/cos2103/Cos2103.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cos2103;
+
+/**
+ *
+ * @author Noojar
+ */
+public class Cos2103 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        f1();
+        int result = f2(5,7);
+        System.out.println("Result: "+result);
+        // TODO code application logic here
+    }
+    public static void f1(){
+    
+    System.out.println("Hello word");
+    
+    }
+    public static int f2(int x,int y){
+        return x+y;
+        
+   
+    
+    }
+}
---
+++ cos2103/src/cos2103/First_array.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cos2103;
+
+/**
+ *
+ * @author Noojar
+ */
+public class First_array {
+    public static void main(String[] args){
+    
+    int DATA[] = new int[10];
+    DATA[0]=23;
+    DATA[1]=45;
+    DATA[2]=5;
+    DATA[3]=7;
+    DATA[4]=28;
+    DATA[5]=55;
+    DATA[6]=78;
+    DATA[7]=89;
+    DATA[8]=90;
+    DATA[9]=1;
+    
+    System.out.println("Array : "+DATA[0]+"\n"+"Array : "+DATA[1]);
+    
+    
+    }
+    
+}
